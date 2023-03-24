306f34bc52ce82c1378e3dd2ab2d7aabefdd8641
hatice <hatice@hotmail.com>
2023-03-24 18:28:09 +0300

    comm

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import SideBar from "@/components/SideBar.vue";
+import ImageGallary from "@/components/ImageGallary.vue";
+import {ref} from "vue";
+
+
+const isWithColor = ref(true);
+
+const onSelectOption = (isColor) => {
+  isWithColor.value= isColor;
+}
+</script>
+
+
+<template>
+  <VApp>
+    <SideBar @selectOption="onSelectOption"/>
+    <VMain>
+      <ImageGallary :isWithColor = "isWithColor" >
+
+      </ImageGallary>
+
+    </VMain>
+  </VApp>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {defineEmits} from 'vue';
+
+const emits = defineEmits(['selectOption'])
+
+const selectOption = (isColor) => {
+  emits("selectOption", isColor)
+}
+</script>
+
+<template>
+  <VNavigationDrawer
+    expand-on-hover
+    rail
+  >
+    <VList>
+      <VListItem
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Sandra Adams"
+          subtitle="sandra_a88@gmailcom"
+      ></VListItem>
+    </VList>
+    <VDivider/>
+    <VList density="compact" nav>
+      <VListItem
+      prepend-icon="mdi-image"
+      title="Color Galery"
+      value="color"
+      @click="selectOption(true)">
+      </VListItem>
+      <VListItem
+          prepend-icon="mdi-dialpad"
+          title="Grayspace Galery"
+          value="color"
+          @click="selectOption(false)"
+      >
+      </VListItem>
+
+    </VList>
+  </VNavigationDrawer>
+</template>
